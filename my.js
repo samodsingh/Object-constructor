cd8625e3f3bb0087608015b56f8b779a2b555a98
samod singh <samod0098@gmail.com>
2021-03-14 10:02:58 +0530

    Object-constructor

---
+++ my.js
@@ -0,0 +1,11 @@
+  var fees = new Object();  // var fees ={};
+  fees["rahul"] = 100;  // fees.rahul=100;
+  fees ["sumit"] =200;   // fees.sumit =200;
+  fees["rohan"] = 300;   // fees.rohan =300;
+
+  fees['total'] = function(){return(100+200);};  
+//   document.write(fees.sumit);
+document.write(fees.total());
+
+
+  
